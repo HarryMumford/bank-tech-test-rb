69f7daaa89727552b949ffbd25a91c62e9bf2b79
HarryMumford <harry_mumford@hotmail.co.uk>
2020-03-16 15:31:40 +0000

    feat: log_withdrawal logs withdrawal

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -6,4 +6,8 @@ class Transaction
   def log_deposit(amount, date = Time.now.stftime("%d/%m/%Y"))
     @log << { type: "deposit", amount: amount, date: date }
   end
+
+  def log_withdrawal(amount, date = Time.now.stftime("%d/%m/%Y"))
+    @log << { type: "withdrawal", amount: amount, date: date }
+  end
 end
--- spec/unit/transaction_spec.rb
+++ spec/unit/transaction_spec.rb
@@ -4,7 +4,7 @@ describe Transaction do
   # let(:statement) { double :statement, :format_statement => 'formatted statement' }
   let(:transaction) { Transaction.new }
 
-  describe '#log' do
+  describe '#log_deposit' do
     it 'logs 1 deposit' do
       expect(transaction.log_deposit(100, "01/01/2020")).to eq([{ type: "deposit", amount: 100, date: "01/01/2020" }])
     end
@@ -16,6 +16,21 @@ describe Transaction do
         { type: "deposit", amount: 100, date: "01/01/2020" }
       ]
       expect(transaction.log_deposit(100, "01/01/2020")).to eq(log)
+    end
+  end
+
+  describe '#log_withdrawal' do
+    it 'logs 1 withdrawal' do
+      expect(transaction.log_withdrawal(100, "01/01/2020")).to eq([{ type: "withdrawal", amount: 100, date: "01/01/2020" }])
+    end
+
+    it 'logs 2 withdrawals' do
+      transaction.log_withdrawal(200, "02/01/2020")
+      log = [
+        { type: "withdrawal", amount: 200, date: "02/01/2020" },
+        { type: "withdrawal", amount: 100, date: "01/01/2020" }
+      ]
+      expect(transaction.log_withdrawal(100, "01/01/2020")).to eq(log)
     end
   end
 end
